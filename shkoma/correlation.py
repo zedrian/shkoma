3782dd89c477b40a07e610c5730db3b4882420f6
zedrian <artsiom.kopats@gmail.com>
2016-02-16 23:19:49 +0300

    correlation: added construct_protein_records(), b2str() moved to utility.

--- shkoma/correlation.py
+++ shkoma/correlation.py
@@ -2,19 +2,17 @@ import uniprot
 from numpy import genfromtxt
 
 from shkoma.peptide import Peptide
+from shkoma.peptide_match import PeptideMatch
+from shkoma.peptide_record import PeptideRecord, find_peptide_record_with_peptide
 from shkoma.protein import Protein, find_protein_with_id
 from shkoma.protein_record import ProteinRecord, find_protein_record_with_protein
+from shkoma.utility import b2str
 
 
 # load all main data as table from .csv file
 def load_main_data_from_csv(file_name):
     main_data = genfromtxt(file_name, dtype=None, delimiter=';', names=True)
     return main_data
-
-
-# simple function converting numpy._bytes to human-readable string
-def b2str(bytes):
-    return str(bytes)[2:-1]
 
 
 # construct list of proteins using main data
@@ -58,30 +56,44 @@ def save_proteins_to_csv(proteins, file_name):
 
 # construct list of protein records using list of proteins and main data
 def construct_protein_records(proteins, main_data):
-    records = []
+    protein_records = []
 
     # 1. process all main data
     for line in main_data:
-        # 1.1. construct peptide from current analysis
-        current_peptide = Peptide(sequence='some sequence')
+        # 1.1. construct peptide and peptide match from current analysis
+        current_peptide = Peptide(sequence=b2str(line['sequence']), pI=line['peptide_pI'])
+        current_peptide_match = PeptideMatch(analysis_name=b2str(line['filename']), score=line['score'],
+                                             reverse_score=line['reverseScore'],
+                                             percent_of_scored_peak_intensity=line['percent_scored_peak_intensity'],
+                                             total_intensity=line['totalIntensity'],
+                                             precursor_averagine_chi_squared=line['precursorAveragineChiSquared'],
+                                             retention_time_min=line['retentionTimeMin'],
+                                             chromatographic_peak_width_in_seconds=line['chromatographicPeakWidthSec'])
 
-        # 1.1. get protein id for current analysis
+        # 1.2. get protein id for current analysis
         current_protein_id = line['accession_number']
 
-        # 1.2. find protein with such id
+        # 1.3. find protein with such id
         protein = find_protein_with_id(proteins,
                                        current_protein_id)  # TODO: if such protein not exists, extract Protein object and add to proteins
 
-        # 1.3. find record with such protein
-        record = find_protein_record_with_protein(records, protein)
+        # 1.4. find record with such protein
+        protein_record = find_protein_record_with_protein(protein_records, protein)
 
-        # 1.4. if record with such protein exists, add current analysis as received_peptide
-        if record is not None:
-            record.received_peptides.append(current_peptide)
-        # ...or create new protein record otherwise
+        # 1.5. if record with such protein exists, add current match to received peptides
+        if protein_record is not None:
+            # 1.5.1. if such peptide was already received, add peptide match
+            peptide_record = find_peptide_record_with_peptide(protein_record.received_peptide_records, current_peptide)
+            if peptide_record is not None:
+                peptide_record.matches.append(current_peptide_match)
+            # 1.5.2. if such peptide was not received yet, add peptide record with this one peptide match
+            else:
+                current_peptide_record = PeptideRecord(current_peptide, [current_peptide_match])
+                protein_record.received_peptide_records.append(current_peptide_record)
+        # 1.6. if protein record with such protein not exists, create new protein record
         else:
-            record = ProteinRecord(protein)
-            record.received_peptides.append(current_peptide)
-            records.append(record)
+            current_peptide_record = PeptideRecord(current_peptide, [current_peptide_match])
+            protein_record = ProteinRecord(protein, received_peptide_records=[current_peptide_record])
+            protein_records.append(protein_record)
 
-    return records
+    return protein_records
---
+++ shkoma/utility.py
@@ -0,0 +1,3 @@
+# simple function converting numpy.bytes_ to human-readable string
+def b2str(bytes):
+    return str(bytes)[2:-1]
