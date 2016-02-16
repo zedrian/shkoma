b8662884cacafd40701841ff991732678a0fad08
zedrian <artsiom.kopats@gmail.com>
2016-02-16 21:13:01 +0300

    correlation: added construct_protein_records().

--- shkoma/correlation.py
+++ shkoma/correlation.py
@@ -1,7 +1,9 @@
 import uniprot
 from numpy import genfromtxt
 
-from shkoma.protein import Protein
+from shkoma.peptide import Peptide
+from shkoma.protein import Protein, find_protein_with_id
+from shkoma.protein_record import ProteinRecord, find_protein_record_with_protein
 
 
 # load all main data as table from .csv file
@@ -35,7 +37,7 @@ def construct_proteins(main_data):
 # load sequences from uniprot.org and fill such field in instances of class Protein
 def fill_protein_sequences(proteins):
     for i in range(0, len(proteins)):
-        print('processing protein #' + str(i + 1) + ' from ' + str(len(proteins)) + '...')
+        print('Processing protein #' + str(i + 1) + ' of ' + str(len(proteins)) + '...')
         # 1. send requests while server will not get correct response
         server_response = None
         while not server_response:
@@ -52,3 +54,34 @@ def save_proteins_to_csv(proteins, file_name):
         for protein in proteins:
             file.write(protein.id + ';' + protein.name + ';' + str(protein.mw) + ';' + str(protein.pI) + ';' +
                        str(protein.M) + ';' + str(protein.Z) + ';' + str(protein.sequence) + '\n')
+
+
+# construct list of protein records using list of proteins and main data
+def construct_protein_records(proteins, main_data):
+    records = []
+
+    # 1. process all main data
+    for line in main_data:
+        # 1.1. construct peptide from current analysis
+        current_peptide = Peptide(sequence='some sequence')
+
+        # 1.1. get protein id for current analysis
+        current_protein_id = line['accession_number']
+
+        # 1.2. find protein with such id
+        protein = find_protein_with_id(proteins,
+                                       current_protein_id)  # TODO: if such protein not exists, extract Protein object and add to proteins
+
+        # 1.3. find record with such protein
+        record = find_protein_record_with_protein(records, protein)
+
+        # 1.4. if record with such protein exists, add current analysis as received_peptide
+        if record is not None:
+            record.received_peptides.append(current_peptide)
+        # ...or create new protein record otherwise
+        else:
+            record = ProteinRecord(protein)
+            record.received_peptides.append(current_peptide)
+            records.append(record)
+
+    return records
